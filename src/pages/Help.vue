80d8a52988692cf736028bd6f55d9c4d76c287b4
dennycio <113514137+dennycio@users.noreply.github.com>
2023-10-31 15:52:20 +0200

    quasar-project

---
+++ src/pages/Help.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Ajuda</h5>
+    <p>Verifique se você tem: Conexão com a Internet (valores do provedor de serviços de Internet podem ser aplicáveis). ...
+Leia os Requisitos do sistema.
+Caso for instalar o sistema operacional pela primeira vez, você precisará da chave do produto Windows (xxxxx-xxxxx-xxxxx-xxxxx-xxxxx). ...
+Inicie a sessão no Centro de Serviços de Licenciamento por Volume para baixar edições Enterprise</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
---
+++ src/pages/PaginaInicial.vue
@@ -0,0 +1,122 @@
+<template>
+  <q-page class="bg-grew-3 column">
+    <div class="classrow q-pa-sa bg-primary">
+      <q-input
+      v-model="newTask"
+      @keyuo.enter="addtask"
+      class="col"
+      square
+      filled
+      bg-color="white"
+       placeholder="Add task"
+       dense>
+        <template v-slot:append>
+          <q-btn
+          @click="addTask"
+          roubd
+          dense
+          flat
+          icon="add" />
+        </template>
+      </q-input>
+    </div>
+    <q-list
+    class="bg-white"
+    separator
+    bordered>
+      <q-item
+       v-for="(task) in tasks"
+       :key="task.title"
+       @click="task.done = !task.done"
+       :class="{'done bg-blue-1' : task.done}"
+       clickable
+       v-ripple>
+        <q-item-section avatar>
+          <q-checkbox
+          v-model="task.done"
+          class="no-pointer-events"
+          color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section
+        v-if="task.done"
+        side>
+        <q-btn
+        @click.stop="deleteTask"
+        flat
+        round
+        dense
+        color="primary"
+        icon="delete" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <div
+     v-if="!tasks.length"
+    class="no-tasks absolute-center">
+        <q-icon
+        name="check"
+        size="100px"
+        color="primary" />
+      <div class="text-h5 text-primary text-center">
+        Sem Conteudo
+        </div>
+     </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: [
+        {
+          title: 'Instalar',
+          done: false
+        },
+        {
+          title: 'BackUp',
+          done: false
+        },
+        {
+          title: 'Drivers',
+          done: false
+        },
+      ]
+    }
+  },
+
+  methods: {
+      deleteTask()
+  {
+
+
+      this.task.splice()
+      this.q.notify(apagado)
+
+    }
+  },
+  addTask () {
+    this.task.push({
+      title: this.newTask,
+      done: false
+    })
+    this.newTask = ''
+  }
+}
+</script>
+
+<style lang="scss">
+.done {
+  .q-item_label {
+text-decoration: line-through;
+color: #bbb
+  }
+}
+.no-tasks {
+  opacity: 0,5;
+}
+</style>
